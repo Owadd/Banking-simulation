089cb09658905fa396bee028c6e5d0d3ae37b46d
Owadd <owadd001@gmail.com>
2024-06-22 13:56:01 +0100

    Updated changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import AccountList from './components/AccountList';
 
 function App() {
-  console.log('App component rendered');
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to the Banking Simulation</h1>
+        <AccountList />
       </header>
     </div>
   );
